75863c8aab5714deb43261b68a14e7f12ddde0a8
Kasto M <99041020+kastomd@users.noreply.github.com>
2026-02-22 15:14:42 -0500

    as

--- app/logic_sub_parts_pmdl/ui_pmdl_sub_parts.py
+++ app/logic_sub_parts_pmdl/ui_pmdl_sub_parts.py
@@ -892,7 +892,6 @@ class MultiSelectTable(ctk.CTkFrame):
         messagebox.showinfo("Elimanado", f"SubPartes {row_idx_old} eliminadas correctamente", parent=self.master.master)
 
     def _uddate_blobs_ui(self):
-        print("bytes reemplazados")
         blob = self.master.master._blobs
         self.master.master.ids_old = []
         # Actualizar los bytes de las parts desde _blob ya normalizado para que las
